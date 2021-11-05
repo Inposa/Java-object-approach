2258bf53fed6f82056895a0fecd64c108bad1ad5
Inposa <donhippo04@gmail.com>
2021-11-05 10:34:11 +0100

    Quick tests on Integer static methods

---
+++ src/com/github/inposa/utils/TestMethodeStatic.java
@@ -0,0 +1,16 @@
+package com.github.inposa.utils;
+
+import java.util.Random;
+
+public class TestMethodeStatic {
+    public static void main(String[] args) {
+        String chaine = "12";
+        int nombre = Integer.parseInt(chaine);
+
+        Random random = new Random();
+        int a = random.nextInt(30);
+        int b = random.nextInt(30);
+        System.out.println("Entre " + a + " et " + b + " le nombre "
+                + Integer.max(a, b) + " est le plus grand !");
+    }
+}
